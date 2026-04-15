a471efa40bbbe3a255ef3c5d195ab38af6d0f725
Michael Gavina <Mikeygavina@yahoo.com>
2026-04-14 22:13:56 -0600

    included main c file

---
+++ cache.c
@@ -0,0 +1,50 @@
+/*
+ * Names:
+ * Michael Gavina
+ * Blake Carbon
+ *
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+
+
+	printf("Enter page reference pattern length: ");
+
+	printf("Enter number of unique pages: ");
+
+	printf("Enter number of slots: ");
+
+	printf("Enter a seed: ");
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
