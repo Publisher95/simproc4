53daf79b7cacada348611790ae3956bc05838728
Michael Gavina <Mikeygavina@yahoo.com>
2026-04-14 23:43:52 -0600

    updated the cache.c file

--- cache.c
+++ cache.c
@@ -21,17 +21,35 @@
 
 int main(){
 
+	int pageSize, pages, slots, rand;
+
 
 	printf("Enter page reference pattern length: ");
+	scanf("%d", &pageSize);
+	while(pageSize > 100 || pageSize < 10){
+		printf("Invalid input\n");
+		printf("Enter page reference pattern length: ");
+		scanf("%d", &pageSize);
+	}
 
 	printf("Enter number of unique pages: ");
+	scanf("%d", &pages);
+	while(pages > 15 || pages < 2){
+		printf("Invalid input\n");
+		printf("Enter number of unique pages: ");
+		scanf("%d", &pages);
+	}
 
 	printf("Enter number of slots: ");
+	scanf("%d", &slots);
+	while(slots < 2 || slots > 10){
+		printf("Invalid input\n");
+		printf("Enter number of slots: ");
+		scanf("%d", &slots);
+	}
 
 	printf("Enter a seed: ");
-
-
-
+	scanf("%d", &rand);
 
 
 
